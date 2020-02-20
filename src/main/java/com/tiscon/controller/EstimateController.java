5a87f2f5b6d0feacb4eeef0298e0b8e778cdaff9
Akari Ohno <akariohno22222@gmail.com>
2020-02-20 13:14:19 +0900

    フォームの分割、遷移の完成

--- src/main/java/com/tiscon/controller/EstimateController.java
+++ src/main/java/com/tiscon/controller/EstimateController.java
@@ -3,6 +3,7 @@ package com.tiscon.controller;
 import com.tiscon.dao.EstimateDao;
 import com.tiscon.dto.UserOrderDto;
 import com.tiscon.form.UserOrderForm;
+import com.tiscon.form.UserEasyForm;
 import com.tiscon.service.EstimateService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
@@ -48,8 +49,8 @@ public class EstimateController {
      */
     @GetMapping("input")
     String input(Model model) {
-        if (!model.containsAttribute("userOrderForm")) {
-            model.addAttribute("userOrderForm", new UserOrderForm());
+        if (!model.containsAttribute("userEasyForm")) {
+            model.addAttribute("userEasyForm", new UserEasyForm());
         }
 
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
@@ -62,9 +63,22 @@ public class EstimateController {
      * @param model 遷移先に連携するデータ
      * @return 遷移先
      */
-    @PostMapping(value = "submit", params = "backToTop")
+    @PostMapping(value = "result", params = "backToTop")
     String backToTop(Model model) {
         return "top";
+    }
+
+    @PostMapping(value = "submit", params = "backToResult")
+    String backToResult(UserEasyForm userEasyForm, Model model) {
+        //料金の計算を行う。
+        UserOrderDto dto = new UserOrderDto();
+        BeanUtils.copyProperties(userEasyForm, dto);
+        Integer price = estimateService.getPrice(dto);
+
+        model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
+        model.addAttribute("userEasyForm", userEasyForm);
+        model.addAttribute("price", price);
+        return "result";
     }
 
     /**
@@ -75,9 +89,9 @@ public class EstimateController {
      * @return 遷移先
      */
     @PostMapping(value = "submit", params = "confirm")
-    String confirm(UserOrderForm userOrderForm, Model model) {
-
+    String confirm(UserEasyForm userEasyForm, UserOrderForm userOrderForm, Model model) {
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
+        model.addAttribute("userEasyForm", userEasyForm);
         model.addAttribute("userOrderForm", userOrderForm);
         return "confirm";
     }
@@ -85,14 +99,14 @@ public class EstimateController {
     /**
      * 入力画面に戻る。
      *
-     * @param userOrderForm 顧客が入力した見積もり依頼情報
+     * @param userEasyForm 顧客が入力した見積もり依頼情報
      * @param model         遷移先に連携するデータ
      * @return 遷移先
      */
-    @PostMapping(value = "result", params = "backToInput")
-    String backToInput(UserOrderForm userOrderForm, Model model) {
+    @PostMapping(value = "detail", params = "backToInput")
+    String backToInput(UserEasyForm userEasyForm, Model model) {
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
-        model.addAttribute("userOrderForm", userOrderForm);
+        model.addAttribute("userEasyForm", userEasyForm);
         return "input";
     }
 
@@ -113,42 +127,48 @@ public class EstimateController {
     /**
      * 概算見積もり画面に遷移する。
      *
-     * @param userOrderForm 顧客が入力した見積もり依頼情報
+     * @param userEasyForm 顧客が入力した見積もり依頼情報
      * @param result        精査結果
      * @param model         遷移先に連携するデータ
      * @return 遷移先
      */
     @PostMapping(value = "result", params = "calculation")
-    String calculation(@Validated UserOrderForm userOrderForm, BindingResult result, Model model) {
+    String calculation(@Validated UserEasyForm userEasyForm, BindingResult result, Model model) {
         if (result.hasErrors()) {
 
             model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
-            model.addAttribute("userOrderForm", userOrderForm);
-            return "confirm";
+            model.addAttribute("userEasyForm", userEasyForm);
+            return "input";
         }
 
         //料金の計算を行う。
         UserOrderDto dto = new UserOrderDto();
-        BeanUtils.copyProperties(userOrderForm, dto);
+        BeanUtils.copyProperties(userEasyForm, dto);
         Integer price = estimateService.getPrice(dto);
 
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
-        model.addAttribute("userOrderForm", userOrderForm);
+        model.addAttribute("userEasyForm", userEasyForm);
         model.addAttribute("price", price);
         return "result";
     }
 
-    @GetMapping(value = "detail")
-    String detail(UserOrderForm userOrderForm, Model model) {
+    @PostMapping(value = "detail", params = "detailInput")
+    String detailInput(UserEasyForm userEasyForm, Model model) {
+        if (!model.containsAttribute("userOrderForm")) {
+            UserOrderForm form = new UserOrderForm();
+            BeanUtils.copyProperties(userEasyForm, form);
+            model.addAttribute("userOrderForm", form);
+        }
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
-        model.addAttribute("userOrderForm", userOrderForm);
+        model.addAttribute("userEasyForm", userEasyForm);
         return "detail";
     }
 
     @PostMapping(value = "order", params = "backToDetail")
-    String backToDetail(UserOrderForm userOrderForm, Model model) {
+    String backToDetail(UserEasyForm userEasyForm, UserOrderForm userOrderForm, Model model) {
         model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
-        model.addAttribute("userDetailForm", userOrderForm);
+        model.addAttribute("userEasyForm", userEasyForm);
+        model.addAttribute("userOrderForm", userOrderForm);
         return "detail";
     }
 
